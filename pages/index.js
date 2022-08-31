a4582c0ec08e578354e1541296a3d6d03019d613
parte-Ajinkya <parte-ajinkya2@gmail.com>
2022-08-31 09:29:36 +0530

    final commit

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head' ;
+import Feed from '../components/Feed';
+import Header from '../components/Header' ;
+import Modal from '../components/Modal';
+
+export default function Home() {
+  return (
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Sticky Header */}
+      <Header/>
+
+      {/* feed */}
+      <Feed/>
+
+      {/* Modal */}
+      <Modal />
+      
+    </div>
+  )
+}
